08a149acec974e8b6fa991214e3ecfc5250c5839
Erik Winter <ik@erikwinter.nl>
2021-01-27 12:58:43 +0100

    smtp is not necessary

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module git.sr.ht/~ewintr/gte
 
 go 1.14
 
-require github.com/emersion/go-imap v1.0.6
+require (
+	github.com/emersion/go-imap v1.0.6
+	github.com/emersion/go-sasl v0.0.0-20200509203442-7bfe0ed36a21
+	github.com/emersion/go-smtp v0.14.0
+)
--- pkg/mstore/email.go
+++ pkg/mstore/email.go
@@ -9,17 +9,12 @@ import (
 
 type EMailStoreConfiguration struct {
 	IMAPURL      string
-	IMAPPort     int
 	IMAPUsername string
 	IMAPPassword string
-	SMTPURL      string
-	SMTPPort     int
-	SMTPUsername string
-	SMTPPassword string
 }
 
 func (esc *EMailStoreConfiguration) Valid() bool {
-	if esc.IMAPURL == "" || esc.IMAPPort == 0 {
+	if esc.IMAPURL == "" {
 		return false
 	}
 	if esc.IMAPUsername == "" || esc.IMAPPassword == "" {
@@ -29,32 +24,37 @@ func (esc *EMailStoreConfiguration) Valid() bool {
 	return true
 }
 
+type smtpConf struct {
+	url string
+	to  string
+}
+
 type EMailStore struct {
-	imapClient *client.Client
+	imap *client.Client
 }
 
 func EMailConnect(conf *EMailStoreConfiguration) (*EMailStore, error) {
-	imapClient, err := client.DialTLS(fmt.Sprintf("%s:%d", conf.IMAPURL, conf.IMAPPort), nil)
+	imap, err := client.DialTLS(conf.IMAPURL, nil)
 	if err != nil {
 		return &EMailStore{}, err
 	}
-	if err := imapClient.Login(conf.IMAPUsername, conf.IMAPPassword); err != nil {
+	if err := imap.Login(conf.IMAPUsername, conf.IMAPPassword); err != nil {
 		return &EMailStore{}, err
 	}
 
 	return &EMailStore{
-		imapClient: imapClient,
+		imap: imap,
 	}, nil
 }
 
-func (es EMailStore) Disconnect() {
-	es.imapClient.Logout()
+func (es *EMailStore) Disconnect() {
+	es.imap.Logout()
 }
 
-func (es EMailStore) Folders() ([]*Folder, error) {
+func (es *EMailStore) Folders() ([]*Folder, error) {
 	boxes, done := make(chan *imap.MailboxInfo), make(chan error)
 	go func() {
-		done <- es.imapClient.List("", "*", boxes)
+		done <- es.imap.List("", "*", boxes)
 	}()
 
 	folders := []*Folder{}
@@ -69,4 +69,35 @@ func (es EMailStore) Folders() ([]*Folder, error) {
 	}
 
 	return folders, nil
+}
+
+func (es *EMailStore) Inbox() ([]*Message, error) {
+	mbox, err := es.imap.Select("INBOX", false)
+	if err != nil {
+		return []*Message{}, err
+	}
+	fmt.Println("Flags for INBOX:", mbox.Flags)
+
+	fmt.Println("Messages: ", mbox.Messages)
+
+	seqset := new(imap.SeqSet)
+	seqset.AddRange(uint32(1), mbox.Messages)
+
+	imsg, done := make(chan *imap.Message), make(chan error)
+	go func() {
+		done <- es.imap.Fetch(seqset, []imap.FetchItem{imap.FetchEnvelope}, imsg)
+	}()
+
+	messages := []*Message{}
+	for m := range imsg {
+		messages = append(messages, &Message{
+			Subject: m.Envelope.Subject,
+		})
+	}
+
+	if err := <-done; err != nil {
+		return []*Message{}, err
+	}
+
+	return messages, nil
 }
